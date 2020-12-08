9257077d8d39f5fe9655c7fa08c411e463099ca4
liuyinleidm@163.com <liuyinleidm@163.com>
2020-12-08 12:57:49 +0800

    kkkk

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,13 @@
 export default {
   data() {
     return {
-      path: ''
+      path: 'pc'
     }
   },
   methods: {
-    _isMobile() {
-      let flag = navigator.userAgent.match(
-        /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-      );
-      return flag;
-    }
   },
 
   created() {
-    if (this._isMobile()) {
-      this.path = 'm';
-    } else {
-      this.path = 'pc'
-    }
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,32 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import index from '../pages/index.vue';
-import mindex from '../pagesApp/index.vue';
 import main from '../pages/main.vue'
 import content from '../pages/content.vue'
-import minmain from '../pagesApp/main.vue'
 Vue.use(VueRouter);
 
 export default new VueRouter({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/pc',
       components: {
         default: index,
-        pc: index,
-        m: mindex
+        pc: index
       }
     },
     {
-      path: '/main',
+      path: '/pc/main',
       components: {
         default: index,
-        pc: main,
-        m: minmain
+        pc: main
       }
     },
     {
-      path: '/content',
+      path: '/pc/content',
       components: {
         default: index,
-        pc: content,
-        m: minmain
+        pc: content
       }
     }
   ]

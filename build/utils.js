cea813d9fa20f36da311229feee0d59e36fd36cd
liuyinleidm@163.com <liuyinleidm@163.com>
2020-12-08 17:05:43 +0800

    kkkk

--- build/utils.js
+++ build/utils.js
@@ -13,7 +13,7 @@ exports.assetsPath = function(_path) {
       ? config.build.assetsSubDirectory
       : config.dev.assetsSubDirectory;
 
-  return path.posix.join('./', assetsSubDirectory, _path);
+  return path.posix.join(assetsSubDirectory, _path);
 };
 
 exports.cssLoaders = function(options) {
